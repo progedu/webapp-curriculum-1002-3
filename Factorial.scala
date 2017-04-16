c7711b63739d0a7a3cc4599d9bbfb4f8e671457c
yu-rie <eleos@eleos.jp>
2017-04-16 12:04:53 +0900

    変数 i を Int 型に

--- Factorial.scala
+++ Factorial.scala
@@ -1,10 +1,13 @@
 import scala.math.BigInt
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = {
-    def factorial(i: BigInt, acc: BigInt): BigInt =
-      if (i <= 1) acc else factorial(i - 1, i * acc)
-    factorial(i, 1)
+  def factorial(i: Int): BigInt = {
+    def factorial(i: Int, acc: BigInt): BigInt = 
+    i match {
+      case 1 => acc
+      case _ => factorial(i - 1, i * acc)
+    }
+    factorial(i , 1)
   }
 
   println(factorial(10000))
