e1058eadd3ebec936d3656f92d2d543ad7aa2f9c
Takashi Udagawa <udtakasi@gmail.com>
2017-04-26 04:37:34 +0900

    スペース入れた

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,7 @@
 import scala.math.BigInt
 
 object FactorialTailRecursion extends App {
-  def factorial(i: BigInt, accum: BigInt): BigInt = if (i == 0) accum else factorial(i-1, accum*i)
+  def factorial(i: BigInt, accum: BigInt): BigInt = if (i == 0) accum else factorial(i - 1, accum * i)
 
   println(factorial(10000, 1))
 }
