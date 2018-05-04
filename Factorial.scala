3e566377751ae4421dd7b115d1727ba636fa1319
storm0017 <yuusuke_17n1100165@nnn.ed.jp>
2018-05-05 06:36:30 +0900

    変数iをInt型に変更

--- Factorial.scala
+++ Factorial.scala
@@ -1,6 +1,6 @@
 import scala.math.BigInt
 
 object FactorialTailRec extends App {
-  def factorial(i: BigInt, acc: BigInt): BigInt = if (i == 0) acc else factorial(i - 1, i * acc) 
+  def factorial(i: Int, acc: BigInt): BigInt = if (i == 0) acc else factorial(i - 1, i * acc) 
   println(factorial(10000, 1))
 }
