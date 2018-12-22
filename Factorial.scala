05a79190188b9fd196371892a97bae3b2b13408b
telos <telos160401@gmail.com>
2018-12-23 03:50:33 +0900

    階乗を計算する関数の末尾再起最適化

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,13 @@
 import scala.math.BigInt
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
-
+  def factorialTailRecusion (i: BigInt, result: BigInt): BigInt = {
+    if (i == 0) { 
+      result 
+    } else {
+      factorialTailRecusion(i - 1, i * result)
+    }
+  }
+  def factorial (i: BigInt): BigInt = factorialTailRecusion(i, 1)
   println(factorial(10000))
 }
