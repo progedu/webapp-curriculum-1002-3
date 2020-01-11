6c546d66160f09ef5e2054c1ac63864200c5021f
近藤康介 <kosuke@k-kosukeMacBook.local>
2020-01-11 22:20:05 +0900

    末尾最適化

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,7 @@
 import scala.math.BigInt
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
-
-  println(factorial(10000))
+  @scala.annotation.tailrec
+  def factorial(i: BigInt, product: BigInt): BigInt = if (i == 0) product else factorial(i - 1, product * i) 
+  println(factorial(1000, 1))
 }
