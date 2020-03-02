caee9e38869b6072d24b9a770dc96cf25fbdf545
hisayuki <siruvalice@gmail.com>
2020-03-02 17:49:41 +0900

    回答済み

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,6 @@
 import scala.math.BigInt
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
-
+  def factorial(i: BigInt, result: BigInt): BigInt = if (i==0) result else  factorial(i-1, i*result)
   println(factorial(10000))
 }
