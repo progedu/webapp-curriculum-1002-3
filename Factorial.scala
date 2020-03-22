873e53383a551c50c33f588d0705eec08041a45f
Takuya Shinozaki <takushino19890913@yahoo.co.jp>
2020-03-22 07:31:32 -0700

    末尾再帰最適化

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,7 @@
 import scala.math.BigInt
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
+  def factorial(i: BigInt, ans:BigInt): BigInt = if (i == 0) ans else factorial(i - 1, ans = i * ans)
 
-  println(factorial(10000))
+  println(factorial(10000,1))
 }
