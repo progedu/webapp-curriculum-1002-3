101b8dea81b48331a47237c0143c98c7fd7f92db
kazumaisamatsu <54142720+kazumaisamatsu@users.noreply.github.com>
2020-05-17 14:30:13 +0900

    スタックオーバフロー改善コーディングの実装

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,6 @@
 import scala.math.BigInt
 
-object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
-
-  println(factorial(10000))
+object FactorialTailRec extends App {
+  def factorial(i: BigInt, acc: BigInt): BigInt = if (i == 0) acc else factorial(i - 1, i * acc) 
+  println(factorial(10000, 1))
 }
