ca2296e61a6075624746bc241fcd198e80bbc95d
orifuji <orfj18@gmail.com>
2020-12-05 00:24:38 +0900

    fix(test_value): 手動でのコンパイル時検査に用いる値が誤っていたため修正した
    ついでに定数化しておいた
    標準入力から受け取るようにしても良かったかもしれない

--- Factorial.scala
+++ Factorial.scala
@@ -1,10 +1,13 @@
 import scala.math.BigInt
 
 object Factorial extends App {
+  // テスト対象とする値を定数化
+  val test_value_for_factorial :Int = 30000
+
   @scala.annotation.tailrec
   def factorial(i: BigInt, acc: BigInt = 1): BigInt =
     if (i == 0) acc
     else factorial(i - 1, i * acc)
 
-  println(factorial(3000))
+  println(factorial(test_value_for_factorial))
 }
