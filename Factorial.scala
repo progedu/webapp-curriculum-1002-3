2bc03aa432b765f6b905240a523e6a2d61a0e610
shama <65126083+oshamashama@users.noreply.github.com>
2022-05-30 17:46:01 +0900

    スタックオーバーフロー が起きるように修正
    relate to issue 884

--- Factorial.scala
+++ Factorial.scala
@@ -3,5 +3,5 @@ import scala.math.BigInt
 object Factorial extends App {
   def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
 
-  println(factorial(10000))
+  println(factorial(30000))
 }
