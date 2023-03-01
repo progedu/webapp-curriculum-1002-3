82a464037ca68ee17472b52bde1965518cb62cc0
diracmax <mkazuki0705@gmail.com>
2023-03-01 21:07:36 +0900

    fix factorial function

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,10 @@
 import scala.math.BigInt
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
+  def factorial(i: BigInt): BigInt = {
+    def factorialTailCall(i: BigInt, accum: BigInt): BigInt = if (i == 0) accum else factorialTailCall(i - 1, i * accum)
+    factorialTailCall(i, 1)
+  }
 
   println(factorial(30000))
 }
