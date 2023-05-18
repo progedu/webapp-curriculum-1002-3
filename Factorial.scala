f6a1a52c036a056a34ba8e34eb5bf475d8f6a512
saitoyoshi <mitti180@gmail.com>
2023-05-18 12:06:24 +0900

    末尾再帰最適化を適用

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,18 @@
 import scala.math.BigInt
+import scala.annotation.tailrec
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
 
+  def factorial(i: BigInt): BigInt = {
+    @tailrec
+    def loop(acc: BigInt, i:BigInt): BigInt = {
+      if (i == 0) {
+        acc
+      } else {
+        loop(acc * i, i - 1)
+      }
+    }
+    loop(1, i)
+  }
   println(factorial(30000))
 }
